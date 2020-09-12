b035355973558776b32cfb01ca19b85681d405cb
ResoTheRed <ltdemg@gmail.com>
2020-09-12 04:39:14 -0700

    Started Job interview files

---
+++ Kati/JobInterview/JobInterviewModule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using Kati.GenericModule;
+
+namespace Kati.JobInterview {
+    public class JobInterviewModule {
+        //any special rules
+        //prevent repeating dialogue
+    }
+}
